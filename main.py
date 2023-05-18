a2df715a930c3a7a4945cfb7ce2bdd63fb6e0554
thfln <109290268+thfln@users.noreply.github.com>
2023-05-18 14:58:55 +0000

    Added a loop to allow the user to play again

--- main.py
+++ main.py
@@ -47,8 +47,14 @@ def checkCave(chosenCave):
         # Need parentheses to call print(). See suggestion below:
       print('Gobbles you down in one bite!')
       
-displayIntro()
-caveNumber = chooseCave()
-checkCave(caveNumber)
-    
-print("Thanks for playing") 
+# Added a play again loop
+playAgain = 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+   displayIntro()
+   caveNumber = chooseCave()
+   checkCave(caveNumber)
+  
+   print('Do you want to play again? (yes or no)')
+   playAgain = input()
+   if playAgain == "no" or playAgain == "n":
+       print("Thanks for playing!")
